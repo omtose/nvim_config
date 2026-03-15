cee5b7bc095d3f640ace402c351a555684d1bff1
omtose <git@oliverdunn.au>
2026-03-15 21:25:35 +1100

    add conform.nvim for format on save with prettier and LSP fallback

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,26 @@
+return {
+	"stevearc/conform.nvim",
+	event = "BufWritePre",
+	cmd = { "ConformInfo" },
+	keys = {
+		{ "<leader>f", function() require("conform").format() end, desc = "Format buffer" },
+	},
+	opts = {
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			htmlangular = { "prettier" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
+}

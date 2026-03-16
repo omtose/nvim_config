ab8e8456f6aa9aa21fd075233fecccc2e31eb00e
omtose <git@oliverdunn.au>
2026-03-16 11:37:27 +1100

    add telescope to code actions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,27 @@
 return {
 	'nvim-telescope/telescope.nvim',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'nvim-telescope/telescope-ui-select.nvim',
+	},
+	event = "VeryLazy",
 	keys = {
 		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
 		{ "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Live Grep" },
 		{ "<leader>fb", "<cmd>Telescope buffers<cr>",  desc = "Open buffers" },
 	},
-	opts = {
-		defaults = {
-			layout_strategy = 'vertical',
-		},
-		pickers = {
-			find_files = {
-				find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+	config = function()
+		local telescope = require("telescope")
+		telescope.setup({
+			defaults = {
+				layout_strategy = 'vertical',
 			},
-		},
-	},
+			pickers = {
+				find_files = {
+					find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+				},
+			},
+		})
+		telescope.load_extension("ui-select")
+	end,
 }

4835b005d3a00a001dbe448cac04cf284db4fa75
omtose <git@oliverdunn.au>
2026-03-16 19:47:13 +1100

    update snacks to use recomended options

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,12 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		indent = { enabled = true },
+		input = { enabled = true },
+		quickfile = { enabled = true },
+		words = { enabled = true },
+	},
+}
